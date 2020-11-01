cb07211dc2adaedf5bee70c40dc1d00649254f84
DeveloperSerhii <example@gmail.com>
2020-11-01 19:54:43 +0200

    add variable and object_

--- HomeWork/script.js
+++ HomeWork/script.js
@@ -23,3 +23,15 @@
 
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+     count: numberOfFilms,
+     movies: {},
+     actors: {},
+     genres: [],
+     privat: false 
+};
+
+
+
