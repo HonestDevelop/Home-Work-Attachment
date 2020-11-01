a3d5f5ba1a450a15dce49120c0c9c818d5d85db1
DeveloperSerhii <example@gmail.com>
2020-11-01 20:02:14 +0200

    add two kid of variables and object with answer

--- HomeWork/script.js
+++ HomeWork/script.js
@@ -33,5 +33,12 @@ const personalMovieDB = {
      privat: false 
 };
 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', '');
+
+const c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies [a] = b;
 
 
